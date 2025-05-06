7e25f420284675467a7e7a0ac1048a39c3ccaec9
Nikhil Saireddy Munagala <nikhilsai.munagala@gmail.com>
2025-05-06 14:41:17 -0400

    Changed format for displaying results when printing.

--- projects/password_checker.py
+++ projects/password_checker.py
@@ -4,21 +4,32 @@
 password = input("Please enter a password: ")
 # Define a set of special characters used for the last check
 special_characters = set("@#$%^&*()-_=+[]{}|;:'\",.<>?/!")
+failed_checks = set() #List of failed checks for the string.
 
 # Check to for password length less than 8 characters.
 if len(password) < 8:
-    print("Your password must be at least 8 characters long.")
+    failed_checks.add("at least 8 characters")
 # Check for 1+ uppercase letters
-elif not any(char.isupper() for char in password):
-    print("Your password needs at least one uppercase letter.")
+if not any(char.isupper() for char in password):
+    failed_checks.add("at least one uppercase letter")
 # Check for 1+ lowercase letters
-elif not any(char.islower() for char in password):
-    print("Your password needs at least one lowercase letter.")
+if not any(char.islower() for char in password):
+    failed_checks.add("at least one lowercase letter")
 # Check for 1+ digits
-elif not any(char.isdigit() for char in password):
-    print("Your password needs at least one digit.")
+if not any(char.isdigit() for char in password):
+    failed_checks.add("at least one digit")
 # Check for 1+ special characters
-elif not any(char in special_characters for char in password):
-    print("Your password needs at least one special character.")
-else:
+if not any(char in special_characters for char in password):
+    failed_checks.add("at least one special character")
+
+#If the user passed all of the checks -> strong password
+if len(failed_checks) == 0:
     print("Your password is strong! 💪") #Passes all of the checks
+else:
+    #Print the first check
+    result = f"Your password needs {failed_checks.pop()}"
+    #If there is still at least one failed check.
+    if len(failed_checks) > 0:
+        failed_checks_list = list(failed_checks) #To peform substring operations.
+        result += f", {', '.join(failed_checks_list[:-1])}, and {failed_checks_list[-1]}"
+    print(result)
